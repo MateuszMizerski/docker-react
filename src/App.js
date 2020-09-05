d79c83fd175fa398c24913d362d2387f9270249f
mateusz Mizerski <mateuszmizerski123@gmail.com>
2020-09-05 08:59:50 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          XD Phylosophy 34
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+     <h2>
+     	coco
+     </h2>
+    </div>
+  );
+}
+
+export default App;
